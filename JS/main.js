2b7dc62cda6f3f0a8982df1f9633c580c8b93c8d
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2022-03-12 22:24:55 -0500

    Drag and Drop working but not properly

--- JS/main.js
+++ JS/main.js
@@ -23,19 +23,57 @@ function playTrack(){
     audioEl.play();
  }
 
+ function dragStarted(event) {
+    console.log("Dragged", this.alt);
 
+    /*	// use the setData method of the drag event to store a reference to the current element
+event.dataTransfer.setData("currentItem", event.target.id);*/
+   // event.dataTransfer.setData("currentItem", event.target.id);
+   event.dataTransfer.setData("text/plain", event.target.id);
+    event.dataTransfer.dropEffect = "move";
+
+}
+
+
+function DragoverFunc(event) {
+    event.preventDefault();
+    console.log("element dragged over me" );
+    event.dataTransfer.dropEffect = "move";
+}
+
+
+function DropFunc(event) {
+    event.preventDefault();
+  
+
+    if(this.childElementCount > 0) {
+        return;
+    }
+
+
+    //let droppedInstrmnt = event.dataTransfer.getData("currentItem");
+    let droppedInstrmntId = event.dataTransfer.getData("text/plain");
+    let droppedInstrmnt = document.getElementById(droppedInstrmntId);
+    console.log ("dropped");
+    this.appendChild(droppedInstrmnt);
+
+//debugger;
+
+} 
 
 
 //calling functions
 
-instruments.forEach(thumb => thumb.addEventListener("click", loadTrack));
-instruments.forEach(piece => piece.addEventListener("dragstart", dragStarted));
 
-dropZones.forEach (zone => {
+instruments.forEach(piece => piece.addEventListener("dragstart", dragStarted));
+instruments.forEach(thumb => thumb.addEventListener("click", loadTrack));
+
+
+dropZones.forEach(zone => {
     zone.addEventListener("dragover", DragoverFunc);
     zone.addEventListener("drop", DropFunc);
 })
 
 
-})()
+})();
 
